7e8ef6b52fa86261de855c67c0619ec4b68cd675
Akshit Sadana <akshitsadana@gmail.com>
2020-04-18 18:37:02 +0530

    states data response key fix

--- src/stats/api.js
+++ src/stats/api.js
@@ -6,10 +6,15 @@ const call = async() => {
 
     const state_data = await fetch('https://api.covid19india.org/data.json');
     const state_obj = await state_data.json();
+    const final_obj = {
+        statewise : state_obj.statewise
+    };
 
-    await fs.writeFile(path.join(__dirname,'../state_data.json'),JSON.stringify(state_obj.statewise),()=>{
+    await fs.writeFile(path.join(__dirname,'../state_data.json'),JSON.stringify(final_obj),()=>{
         console.log('state saved');
     });
 };
+
+call()
 
 module.exports = call;
