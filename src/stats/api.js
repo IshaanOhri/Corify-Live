cd5a451a40e95a6f3e8880d6aff74988ed0422c9
Akshit Sadana <akshitsadana@gmail.com>
2020-04-18 18:10:21 +0530

    states data fix

--- src/stats/api.js
+++ src/stats/api.js
@@ -5,10 +5,13 @@ const fs = require('fs');
 const call = async() => {
 
     const state_data = await fetch('https://api.covid19india.org/data.json');
+    const state_obj = await state_data.json();
 
-    await fs.writeFile(path.join(__dirname,'../state_data.json'),JSON.stringify(state_data),()=>{
+    await fs.writeFile(path.join(__dirname,'../state_data.json'),JSON.stringify(state_obj.statewise),()=>{
         console.log('state saved');
     });
 };
+
+call();
 
 module.exports = call;
